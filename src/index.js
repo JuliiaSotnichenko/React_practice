2ee9ebfea54cac274739ed6b48f6edc881023053
yuliia_sotnichenko <juliia.sotnichenko@gmail.com>
2021-07-03 23:21:07 +0200

    state and props

--- src/index.js
+++ src/index.js
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import  App from './components/app/';
 
-
-function WhoAmI (props){
-  return (
-    <React.Fragment>
-          <h1>My name is {props.name} , surname - {props.surname} </h1>
-           <a href={props.link}>My profile</a>
-    </React.Fragment>
-    
+class WhoAmI extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      years: 30
+    }
+    this.nextYear = this.nextYear.bind(this);
+  }
+    nextYear(){
+      this.setState(state => ({
+          years: ++state.years
+      }))
+          
+    }
+  render() {
+    const {name, surname, link} = this.props;
+    const {years} = this.state;
+    return (
+      
+        < >
+          <button onClick={this.nextYear}>++</button>
+          <h1>My name is {name} , surname - {surname}, years - {this.state.years} </h1>
+           <a href={link}>My profile</a>
+    </>
+  
+    )
+  }
+}
+ const All =() =>{
+  return(
+    <>
+    <WhoAmI name="Juliia" surname="Soty" link="facebook.com" />
+    <WhoAmI name="John" surname="Black" link="facebook.com" />
+    <WhoAmI name="Vita" surname="Vit" link="facebook.com" />
+    </>
   )
 }
 
 
 
-
-
-ReactDOM.render(
-  <React.StrictMode>
-    <WhoAmI name= "Yulia" surname="Soty" link="facebook.com"/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<All/>, document.getElementById('root'));
 
